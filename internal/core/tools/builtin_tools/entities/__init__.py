88d42e0d98a727c62e8e4949c80d4b262657b166
caixiaorong <smart_r@163.com>
2024-09-22 23:41:34 +0800

    插件广场分类与图标接口实现

--- internal/core/tools/builtin_tools/entities/__init__.py
+++ internal/core/tools/builtin_tools/entities/__init__.py
@@ -5,7 +5,8 @@
 @Author : rxccai@gmail.com
 @File   : __init__.py.py
 """
+from .category_entity import CategoryEntity
 from .provider_entity import ProviderEntity, Provider
 from .tool_entity import ToolEntity
 
-__all__ = ["Provider", "ProviderEntity", "ToolEntity"]
+__all__ = ["Provider", "ProviderEntity", "ToolEntity", "CategoryEntity"]
--- internal/service/builtin_tool_service.py
+++ internal/service/builtin_tool_service.py
@@ -5,11 +5,16 @@
 @Author : rxccai@gmail.com
 @File   : builtin_tool_service.py
 """
+import mimetypes
+import os.path
 from dataclasses import dataclass
+from typing import Any
 
+from flask import current_app
 from injector import inject
 from langchain_core.pydantic_v1 import BaseModel
 
+from internal.core.tools.builtin_tools.categories import BuiltinCategoryManager
 from internal.core.tools.builtin_tools.providers import BuiltinProviderManager
 from internal.exception import NotFoundException
 
@@ -19,6 +24,7 @@ from internal.exception import NotFoundException
 class BuiltinToolService:
     """内置工具服务"""
     builtin_provider_manager: BuiltinProviderManager
+    builtin_category_manager: BuiltinCategoryManager
 
     def get_builtin_tools(self) -> list:
         """获取所有内置提供商和工具对应信息"""
@@ -64,6 +70,33 @@ class BuiltinToolService:
             "inputs": self.get_tool_inputs(tool)
         }
         return builtin_tool
+
+    def get_provider_icon(self, provider_name: str) -> tuple[bytes, str]:
+        # 获取提供商
+        provider = self.builtin_provider_manager.get_provider(provider_name)
+        if not provider:
+            raise NotFoundException(f"该提供商{provider_name}不存在")
+        # 获取根目录信息
+        root_path = os.path.dirname(os.path.dirname(current_app.root_path))
+        provider_path = os.path.join(root_path, "internal", "core", "tools", "builtin_tools", "providers",
+                                     provider_name)
+        icon_path = os.path.join(provider_path, "_asset", provider.provider_entity.icon)
+
+        if not os.path.exists(icon_path):
+            raise NotFoundException(f"图标不存在")
+        mimetype, _ = mimetypes.guess_type(icon_path)
+        mimetype = mimetype or "application/octet-stream"
+        with open(icon_path, "rb") as f:
+            byte_data = f.read()
+            return byte_data, mimetype
+
+    def get_categories(self) -> list[dict[str, Any]]:
+        category_map = self.builtin_category_manager.get_category_map()
+        return [{
+            "name": category["entity"].name,
+            "category": category["entity"].category,
+            "icon": category["icon"]
+        } for category in category_map.values()]
 
     @classmethod
     def get_tool_inputs(cls, tool) -> list:
