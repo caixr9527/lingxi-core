ded74b3cc9f581ce5443b10597eacfc215407ff7
caixiaorong <smart_r@163.com>
2024-09-24 23:26:11 +0800

    API插件数据库表设计与ORM实现

--- internal/model/__init__.py
+++ internal/model/__init__.py
@@ -5,6 +5,7 @@
 @Author : rxccai@gmail.com
 @File   : __init__.py.py
 """
+from .api_tool import ApiTool, ApiToolProvider
 from .app import App
 
-__all__ = [App]
+__all__ = ["App", "ApiTool", "ApiToolProvider"]
