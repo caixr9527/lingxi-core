3badab24afb76233b6cf69398d129d2d56d3d6a6
caixiaorong <smart_r@163.com>
2024-10-14 23:28:23 +0800

    文件上传模块数据库表设计

--- internal/model/__init__.py
+++ internal/model/__init__.py
@@ -7,5 +7,6 @@
 """
 from .api_tool import ApiTool, ApiToolProvider
 from .app import App
+from .upload_file import UploadFile
 
-__all__ = ["App", "ApiTool", "ApiToolProvider"]
+__all__ = ["App", "ApiTool", "ApiToolProvider", "UploadFile"]
