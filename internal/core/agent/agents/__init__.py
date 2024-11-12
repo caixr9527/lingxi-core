47d0d527a1232c617abe3e72d23e26ae2f0d0229
caixiaorong <smart_r@163.com>
2024-11-12 22:56:02 +0800

    Agent图结构节点完善与子线程优化

--- internal/core/agent/agents/__init__.py
+++ internal/core/agent/agents/__init__.py
@@ -5,3 +5,7 @@
 @Author : rxccai@gmail.com
 @File   : __init__.py.py
 """
+from .base_agent import BaseAgent
+from .function_call_agent import FunctionCallAgent
+
+__all__ = ["BaseAgent", "FunctionCallAgent"]
--- internal/core/agent/agents/function_call_agent.py
+++ internal/core/agent/agents/function_call_agent.py
@@ -5,14 +5,17 @@
 @Author : rxccai@gmail.com
 @File   : function_call_agent.py
 """
+import json
+from threading import Thread
 from typing import Literal
 
-from langchain_core.messages import AnyMessage, HumanMessage
+from langchain_core.messages import AnyMessage, HumanMessage, SystemMessage, RemoveMessage, ToolMessage
 from langgraph.constants import END
 from langgraph.graph import StateGraph
 from langgraph.graph.state import CompiledGraph
 
-from internal.core.agent.entities.agent_entity import AgentState
+from internal.core.agent.entities.agent_entity import AgentState, AGENT_SYSTEM_PROMPT_TEMPLATE
+from internal.exception import FailException
 from .base_agent import BaseAgent
 
 
@@ -23,12 +26,15 @@ class FunctionCallAgent(BaseAgent):
         if history is None:
             history = []
         agent = self._build_agent()
-
-        return agent.invoke({
-            "messages": [HumanMessage(content=query)],
-            "history": history,
-            "long_term_memory": long_term_memory,
-        })
+        thread = Thread(target=agent.invoke,
+                        args=(
+                            {
+                                "messages": [HumanMessage(content=query)],
+                                "history": history,
+                                "long_term_memory": long_term_memory,
+                            },
+                        ))
+        thread.start()
 
     def _build_agent(self) -> CompiledGraph:
         graph = StateGraph(AgentState)
@@ -47,13 +53,58 @@ class FunctionCallAgent(BaseAgent):
 
     def _long_term_memory_recall_node(self, state: AgentState) -> AgentState:
         """长期记忆召回节点"""
-        pass
+        long_term_memory = ""
+        if self.agent_config.enable_long_term_memory:
+            long_term_memory = state["long_term_memory"]
+
+        preset_messages = [
+            SystemMessage(AGENT_SYSTEM_PROMPT_TEMPLATE.format(
+                preset_prompt=self.agent_config.preset_prompt,
+                long_term_memory=long_term_memory,
+            ))
+        ]
+        history = state["history"]
+        if isinstance(history, list) and len(history) > 0:
+            if len(history) % 2 != 0:
+                raise FailException("智能体历史消息列表格式错误")
+            preset_messages.extend(history)
+        human_message = state["messages"][-1]
+        preset_messages.append(HumanMessage(human_message.content))
+
+        return {
+            "messages": [RemoveMessage(id=human_message.id), *preset_messages],
+        }
 
     def _llm_node(self, state: AgentState) -> AgentState:
-        pass
+        llm = self.agent_config.llm
+        if hasattr(llm, "bind_tools") and callable(getattr(llm, "bind_tools")) and len(self.agent_config.tools) > 0:
+            llm = llm.bind_tools(self.agent_config.tools)
+
+        gathered = None
+        is_first_chunk = True
+        for chunk in llm.stream(state["messages"]):
+            if is_first_chunk:
+                gathered = chunk
+                is_first_chunk = False
+            else:
+                gathered += chunk
+        return {"messages": [gathered]}
 
     def _tools_node(self, state: AgentState) -> AgentState:
-        pass
+        tools_by_name = {tool.name: tool for tool in self.agent_config.tools}
+
+        tool_calls = state["messages"][-1].tool_calls
+
+        messages = []
+        for tool_call in tool_calls:
+            tool = tools_by_name[tool_call["name"]]
+            tool_result = tool.invoke(tool_call["args"])
+            messages.append(ToolMessage(
+                tool_call_id=tool_call["id"],
+                content=json.dumps(tool_result),
+                name=tool_call["name"],
+            ))
+        return {"messages": messages}
 
     @classmethod
     def _tools_condition(cls, state: AgentState) -> Literal["tools", "__end__"]:
