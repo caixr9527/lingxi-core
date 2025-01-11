3e515d35048d834c05281c3af32905291019fbdc
caixiaorong <smart_r@163.com>
2025-01-11 10:33:57 +0800

    代码优化

--- internal/core/workflow/nodes/end/end_node.py
+++ internal/core/workflow/nodes/end/end_node.py
@@ -10,9 +10,9 @@ from typing import Optional
 from langchain_core.runnables import RunnableConfig
 
 from internal.core.workflow.entities.node_entity import NodeResult, NodeStatus
-from internal.core.workflow.entities.variable_entity import VariableValueType, VARIABLE_TYPE_DEFAULT_VALUE_MAP
 from internal.core.workflow.entities.workflow_entity import WorkflowState
 from internal.core.workflow.nodes import BaseNode
+from internal.core.workflow.utils.helper import extract_variables_from_state
 from .end_entity import EndNodeData
 
 
@@ -22,20 +22,7 @@ class EndNode(BaseNode):
 
     def invoke(self, state: WorkflowState, config: Optional[RunnableConfig] = None) -> WorkflowState:
         # 提取数据
-        outputs = self.node_data.outputs
-
-        outputs_dict = {}
-        for output in outputs:
-            if output.value.type == VariableValueType.LITERAL:
-                outputs_dict[output.name] = output.value.content
-            else:
-                # 引用数据类型
-                for node_result in state["node_results"]:
-                    if node_result.node_data.id == output.value.content.ref_node_id:
-                        outputs_dict[output.name] = node_result.outputs.get(
-                            output.value.content.ref_var_name,
-                            VARIABLE_TYPE_DEFAULT_VALUE_MAP.get(output.type),
-                        )
+        outputs_dict = extract_variables_from_state(self.node_data.outputs, state)
         # 组装并返回
         return {
             "outputs": outputs_dict,
