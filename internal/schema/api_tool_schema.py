ed1a97526a090dce9440055b8b288a917b58dcbf
caixiaorong <smart_r@163.com>
2025-02-09 17:13:03 +0800

    代码优化

--- internal/schema/api_tool_schema.py
+++ internal/schema/api_tool_schema.py
@@ -82,8 +82,8 @@ class GetApiToolProviderResp(Schema):
     name = fields.String()
     icon = fields.String()
     openapi_schema = fields.String()
-    headers = fields.List(fields.Dict, default=[])
-    created_at = fields.Integer(default=0)
+    headers = fields.List(fields.Dict, dump_default=[])
+    created_at = fields.Integer(dump_default=0)
 
     @pre_dump
     def process_data(self, data: ApiToolProvider, **kwargs):
@@ -101,7 +101,7 @@ class GetApiToolResp(Schema):
     id = fields.UUID()
     name = fields.String()
     description = fields.String()
-    inputs = fields.List(fields.Dict, default=[])
+    inputs = fields.List(fields.Dict, dump_default=[])
     provider = fields.Dict()
 
     @pre_dump
@@ -127,9 +127,9 @@ class GetApiToolProvidersWithPageResp(Schema):
     name = fields.String()
     icon = fields.String()
     description = fields.String()
-    headers = fields.List(fields.Dict, default=[])
-    tools = fields.List(fields.Dict, default=[])
-    created_at = fields.Integer(default=0)
+    headers = fields.List(fields.Dict, dump_default=[])
+    tools = fields.List(fields.Dict, dump_default=[])
+    created_at = fields.Integer(dump_default=0)
 
     @pre_dump
     def process_data(self, data: ApiToolProvider, **kwargs):
