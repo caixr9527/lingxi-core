185fd1439b45bc4864fdc74975dd6cec65d57ef4
caixiaorong <smart_r@163.com>
2025-05-19 17:01:00 +0800

    添加文件识别工具

--- internal/core/tools/builtin_tools/providers/extract/content_extract.py
+++ internal/core/tools/builtin_tools/providers/extract/content_extract.py
@@ -20,8 +20,12 @@
 """
 from typing import Type, Any
 
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_core.tools import BaseTool
+from langchain_openai import ChatOpenAI
 
 from internal.lib.helper import add_attribute, get_file_extension
 
@@ -41,7 +45,7 @@ class ContentExtractTool(BaseTool):
     prompt_template = """
     你是一位专业的图片文本内容提取大师，能够准确、高效地从图片中提取完整的文本内容信息。
     可以根据用户提供的图片，精准提取图片所包含的信息，仅输出图片中的文本内容，不添加多余的描述与修饰，图片文本内容是什么就输出什么。
-    如果图片中无文本文字信息，则如实告诉用户图片文本内容为空，不对图片进行描述。
+    如果图片中无文本文字信息，则如实告诉用户图片文本内容为空，不对图片进行过多的修饰。
     """
 
     def _run(self, *args: Any, **kwargs: Any) -> str:
@@ -55,8 +59,20 @@ class ContentExtractTool(BaseTool):
                 file_extractor = injector.get(FileExtractor)
                 return file_extractor.load_from_url(url, return_text=True)
             elif ext in self.img_ext:
-                # todo generate_conversation_name
-                return ""
+                prompt = ChatPromptTemplate.from_messages([
+                    SystemMessage(content=self.prompt_template),
+                    HumanMessage(content=[
+                        {"type": "text", "text": "请准确的提取图片链接里面的内容"},
+                        {"type": "image_url", "image_url": {"url": url}},
+                    ])
+                ])
+                llm = ChatOpenAI(
+                    model="gpt-4o",
+                    temperature=0,
+                    max_tokens=16384,
+                )
+                chain = prompt | llm | StrOutputParser()
+                return chain.invoke({})
             else:
                 return f"文档格式：{ext} 不支持内容提取"
         except Exception as e:
