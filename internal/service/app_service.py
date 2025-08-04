aa2684001a667310641c67b5adf44f234b65eabc
caixiaorong <smart_r@163.com>
2025-08-04 23:03:50 +0800

    #95

--- internal/service/app_service.py
+++ internal/service/app_service.py
@@ -376,7 +376,7 @@ class AppService(BaseService):
         app = self.get_app(app_id, account)
 
         # 检测下当前应用的状态是否为已发布
-        if app.status != AppStatus.PUBLISHED:
+        if app.status == AppStatus.DRAFT:
             raise FailException("当前应用未发布，请核实后重试")
 
         # 修改账号的发布状态，并清空关联配置id
